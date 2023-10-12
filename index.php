f0ee46763544d9c0091012c9d31903f417ac6396
Kondro571 <133992948+Kondro571@users.noreply.github.com>
2023-10-12 17:37:42 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
 
-echo 'Hiii there 👋';
+<html>
+    <head>
+    <title>Tak</title>
+        <meta charset="utf-8">
+        <meta name="description" content="My bisness">
+        <meta name="author" content="Konrad Tatomir">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+        
+    </head>
+    <body>
+        <div id="login">
+        <form action="" method="post" enctype="text/plain">
+        <input name="email" placeholder="Email" id="email"><br>
+        <input name="password" placeholder="Password" id="password">
+        <input type="button" name="Log in" id="login">
+        <?php
+
+       
+
+        ?>
+        <form>
+        <div>
+
+    </body>
+</html>
